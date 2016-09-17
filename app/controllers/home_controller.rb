24092366694eed45520bec62c60e1f6e3f0c7fe9
Geilton Xavier <geilton@live.com>
2016-09-17 19:27:50 -0300

    Extract import logic from home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,26 +1,7 @@
 class HomeController < ApplicationController
   def index
     render :index, locals: {
-      makes: find_all_makes
+      makes: MakeService.new.find_all_makes
     }
-  end
-
-  protected
-
-  def find_all_makes
-    uri = URI('http://www.webmotors.com.br/carro/marcas')
-
-    response = Net::HTTP.post_form(uri, {})
-    json = JSON.parse response.body
-
-    webmotors_makes = json.map { |h| [h['Nome'], h['Id']] }.to_h
-    found_makes = Make.where(name: webmotors_makes.keys).pluck(:name)
-    makes_to_import = webmotors_makes.keys - found_makes
-    insert_params = makes_to_import.map do |make|
-      { name: make, webmotors_id: webmotors_makes[make] }
-    end
-    ActiveRecord::Base.transaction { Make.create(insert_params) }
-
-    Make.all
   end
 end
--- app/models/make.rb
+++ app/models/make.rb
@@ -1,2 +1,9 @@
 class Make < ActiveRecord::Base
+  def self.import(makes)
+    transaction { create(makes) }
+  end
+
+  def self.find_existing_names(names)
+    where(name: names).pluck(:name)
+  end
 end

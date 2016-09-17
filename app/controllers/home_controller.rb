0ec62d656163ed45e5a9bb0d57dd1017aef77b0b
Geilton Xavier <geilton@live.com>
2016-09-17 17:28:50 -0300

    Extract view dependency to local variable

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,13 +7,15 @@ class HomeController < ApplicationController
     response = Net::HTTP.post_form(uri, {})
     json = JSON.parse response.body
 
-    debugger
-
     # Itera no resultado e grava as marcas que ainda não estão persistidas
     json.each do |make_params|
       if Make.where(name: make_params["Nome"]).size == 0
         Make.create(name: make_params["Nome"], webmotors_id: make_params["Id"])
       end
     end
+
+    render :index, locals: {
+      makes: Make.all
+    }
   end
 end
